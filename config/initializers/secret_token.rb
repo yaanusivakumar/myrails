aeb3b239fa4303b818974a4d09e69e80decb5d2c
test1 <test@test-HP-Compaq-dc7800-Convertible-Minitower.(none)>
2012-11-07 19:51:50 +0530

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'a7cca6913d4a0cbd9f5763daf5cb2e1af42d8591e0daf3cd3d50bf8ed263ea82b8ea68c153888389c876b07797bad5c70a2d50c7f07d4590b86df432eda54638'
